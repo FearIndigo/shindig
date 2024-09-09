4f67eea60d5418eb916db82001f55d27b16d407a
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-10 02:07:51 +0800

    -Update cdk stack imports and make s3 bucket private -add long cache policy to frontend files

--- cdk/lib/cdk-stack.js
+++ cdk/lib/cdk-stack.js
@@ -1,14 +1,21 @@
+const { Stack, RemovalPolicy, Duration } = require("aws-cdk-lib");
 const {
-  Stack,
-  RemovalPolicy,
-  aws_s3,
-  aws_s3_deployment,
-  aws_cloudfront,
-} = require("aws-cdk-lib");
-const { S3Origin } = require("aws-cdk-lib/aws-cloudfront-origins");
-const { ViewerProtocolPolicy } = require("aws-cdk-lib/aws-cloudfront");
-const { Source } = require("aws-cdk-lib/aws-s3-deployment");
+  Bucket,
+  BlockPublicAccess,
+  BucketAccessControl,
+} = require("aws-cdk-lib/aws-s3");
+const {
+  OriginAccessIdentity,
+  Distribution,
+  ViewerProtocolPolicy,
+} = require("aws-cdk-lib/aws-cloudfront");
+const {
+  BucketDeployment,
+  Source,
+  CacheControl,
+} = require("aws-cdk-lib/aws-s3-deployment");
 const path = require("path");
+const { S3Origin } = require("aws-cdk-lib/aws-cloudfront-origins");
 
 class CdkStack extends Stack {
   /**
@@ -23,36 +30,44 @@ class CdkStack extends Stack {
     // Retrieve ssl certificate from custom props.
     const certificate = customProps.certificate;
 
-    // Create s3 bucket to store static frontend files (website and images).
-    const bucket = new aws_s3.Bucket(this, "Shindig_Bucket", {
+    // Create private s3 bucket to store static frontend files (website and images).
+    const bucket = new Bucket(this, "Shindig_Bucket", {
       enforceSSL: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      accessControl: BucketAccessControl.PRIVATE,
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
-    // Create cloudfront distribution. (NOTE: DNS records must be updated to point your custom domain to this cloudfront distribution.)
-    const distribution = new aws_cloudfront.Distribution(
+    // Create origin access identity to allow cloudfront distribution to access s3 bucket.
+    const originAccessIdentity = new OriginAccessIdentity(
       this,
-      "Shindig_Distribution",
-      {
-        certificate,
-        defaultRootObject: "index.html",
-        defaultBehavior: {
-          origin: new S3Origin(bucket),
-          viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        },
-        domainNames: [customProps.domainName],
-      }
+      "Shindig_OriginAccessIdentity"
     );
 
+    // Grant bucket read access to the origin access identity used by cloudfront.
+    bucket.grantRead(originAccessIdentity);
+
+    // Create cloudfront distribution. (NOTE: DNS records must be updated to route your custom domain to this cloudfront distribution.)
+    const distribution = new Distribution(this, "Shindig_Distribution", {
+      certificate,
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: new S3Origin(bucket, { originAccessIdentity }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      domainNames: [customProps.domainName],
+    });
+
     // Deploy frontent to s3 bucket. (NOTE: frontend files need to be generated first.)
-    const bucketDeployment = new aws_s3_deployment.BucketDeployment(
+    const bucketDeployment = new BucketDeployment(
       this,
       "Shindig_BucketDeployment",
       {
         destinationBucket: bucket,
         sources: [Source.asset(path.resolve(__dirname, "../../frontend/dist"))],
         distribution,
+        cacheControl: [CacheControl.maxAge(Duration.days(365))],
       }
     );
   }
