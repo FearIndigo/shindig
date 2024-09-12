e49427453c2e1fb3071d24477e68d76e5830030f
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-12 23:12:32 +0800

    remove user REST endpoints

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,12 +1,7 @@
+/**@param {import("rxdb-server").RxServer} rxServer  */
 export default function addUserRoutes(rxServer) {
   // Replication endpoint.
   rxServer.addReplicationEndpoint({
-    name: "user",
-    collection: rxServer.database.collections.users,
-  });
-
-  // REST endpoints.
-  rxServer.addRestEndpoint({
     name: "user",
     collection: rxServer.database.collections.users,
   });
