fefc718ff439eb18c38b301d03b221328e2f5b60
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-12 17:15:12 +0800

    add response collection

--- backend/src/rxdb/collections/index.js
+++ backend/src/rxdb/collections/index.js
@@ -1,13 +1,15 @@
-import users from "./userCollection.js";
-import events from "./eventCollection.js";
 import comments from "./commentCollection.js";
+import events from "./eventCollection.js";
 import interactions from "./interactionCollection.js";
+import responses from "./responseCollection.js";
+import users from "./userCollection.js";
 
 export default async function addCollections(db) {
   await db.addCollections({
-    users,
-    events,
     comments,
+    events,
     interactions,
+    responses,
+    users,
   });
 }
---
+++ backend/src/rxdb/collections/responseCollection.js
@@ -0,0 +1,5 @@
+import responseSchema from "../schemas/responseSchema.js";
+
+export default {
+  schema: responseSchema,
+};
