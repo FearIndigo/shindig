e1ce9587fc622b4b9f0b805425f81336e9c0dcd3
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-16 14:45:01 +0800

    Fix auth handler

--- backend/src/middleware/authHandler.js
+++ backend/src/middleware/authHandler.js
@@ -3,24 +3,32 @@ import signature from "cookie-signature";
 import { store } from "./session.js";
 
 export default async function authHandler(headers) {
-  // Get session id from cookie header.
-  const sessionCookie = cookie.parse(headers.cookie)["connect.sid"];
-  const sessionId = signature.unsign(
-    sessionCookie.slice(2),
-    process.env.AUTH_SECRET
-  );
+  let session;
+  let validUntil = Date.now() * 2;
 
-  // Get session from store.
-  const session = await new Promise((res, rej) => {
-    store.get(sessionId, (e, s) => {
-      res(s);
-    });
-  });
+  if (headers.cookie) {
+    // Get session id from cookie header.
+    const sessionCookie = cookie.parse(headers.cookie)["connect.sid"];
 
-  // Get session valid until.
-  const validUntil = session
-    ? new Date(session.cookie.expires).getTime()
-    : Date.now() * 2;
+    if (sessionCookie) {
+      const sessionId = signature.unsign(
+        sessionCookie.slice(2),
+        process.env.AUTH_SECRET
+      );
+
+      // Get session from store.
+      session = await new Promise((res, rej) => {
+        store.get(sessionId, (e, s) => {
+          res(s);
+        });
+      });
+
+      if (session) {
+        // Get session valid until.
+        validUntil = new Date(session.cookie.expires).getTime();
+      }
+    }
+  }
 
   return {
     data: {
