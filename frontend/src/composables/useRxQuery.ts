eec3050be5a14a6c3943c3bf4876be340b3ee400
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-17 12:55:13 +0800

    use collection type in useRxQuery

--- frontend/src/composables/useRxQuery.ts
+++ frontend/src/composables/useRxQuery.ts
@@ -4,12 +4,14 @@ import type { DatabaseCollections } from "~/rxdb/types";
 
 export default async function useRxQuery<DocType, ReturnType>(
   collectionName: keyof DatabaseCollections,
-  queryBuilder: (collection: RxCollection) => RxQuery<DocType, ReturnType>
+  queryBuilder: (
+    collection: RxCollection<DocType>
+  ) => RxQuery<DocType, ReturnType>
 ) {
   const db = await useRxDB();
   const getKeyValue = <T extends object>(obj: T, key: keyof T) => obj[key];
   const collection = getKeyValue(db.collections, collectionName);
-  const query = queryBuilder(collection);
+  const query = queryBuilder(collection as RxCollection<DocType>);
   const result = useObservable(query.$);
   return result;
 }
