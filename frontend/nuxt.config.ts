c7964ff68f38e9f388b0961d0b1dc9f03ed1a57d
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-17 20:12:56 +0800

    add vuetify and other nuxt modules

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,16 +2,32 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  ssr: true, // Pre-render routes.
+
+  // Pre-render routes.
+  ssr: true,
+
   srcDir: "src/",
+
   router: {
     options: {
       scrollBehaviorType: "smooth",
     },
   },
+
   runtimeConfig: {
     public: {
       apiBase: "",
+    },
+  },
+
+  modules: ["@vueuse/nuxt", "vuetify-nuxt-module"],
+
+  vuetify: {
+    moduleOptions: {},
+    vuetifyOptions: {
+      theme: {
+        defaultTheme: "dark",
+      },
     },
   },
 });
