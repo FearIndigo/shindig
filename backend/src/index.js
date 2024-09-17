71725f67d618d3a0371ecbab6a4f5e4f10cf15bb
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-17 11:03:13 +0800

    set backend cors allowed origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { RxServerAdapterExpress } from "rxdb-server/plugins/adapter-express";
 import authHandler from "./middleware/authHandler.js";
 import addMiddleware from "./middleware/index.js";
 import addRoutes from "./routes/index.js";
+import cors from "cors";
 
 // Initialise RxDB.
 const db = await initRxDB();
@@ -15,6 +16,16 @@ const rxServer = await createRxServer({
   port: 443,
   authHandler,
 });
+
+let allowedOrigins = ["http://localhost:443", "http://localhost:3000"];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions = {
+    origin: allowedOrigins.includes(req.header("Origin")),
+    credentials: true,
+  };
+  callback(null, corsOptions);
+};
+rxServer.serverApp.use(cors(corsOptionsDelegate));
 
 // Add middleware.
 await addMiddleware(rxServer);
