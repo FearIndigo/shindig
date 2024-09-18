02b8611eaf047f530f1b31282499a4351314f21c
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-18 12:51:49 +0800

    Update login and logout forms

--- frontend/src/components/form/LoginForm.vue
+++ frontend/src/components/form/LoginForm.vue
@@ -1,5 +1,15 @@
 <template>
-  <form :action="`${$config.public.apiBase}/auth/login`">
-    <v-btn type="submit">Log in with Keycloak</v-btn>
-  </form>
+  <v-card
+    prepend-icon="mdi-login"
+    title="Log In"
+    text="Select your login method."
+    variant="tonal"
+    max-width="320"
+  >
+    <v-card-actions class="w-100">
+      <form :action="`${$config.public.apiBase}/auth/login`" class="w-100">
+        <v-btn type="submit" variant="tonal" block>Log in with Keycloak</v-btn>
+      </form>
+    </v-card-actions>
+  </v-card>
 </template>
--- frontend/src/components/form/LogoutForm.vue
+++ frontend/src/components/form/LogoutForm.vue
@@ -1,5 +1,19 @@
 <template>
-  <form :action="`${$config.public.apiBase}/auth/logout`" method="post">
-    <v-btn type="submit">Log out</v-btn>
-  </form>
+  <v-card
+    prepend-icon="mdi-logout"
+    title="Log Out"
+    text="Are you sure you want to log out?"
+    variant="tonal"
+    max-width="320"
+  >
+    <v-card-actions class="w-100">
+      <form
+        :action="`${$config.public.apiBase}/auth/logout`"
+        method="post"
+        class="w-100"
+      >
+        <v-btn type="submit" variant="tonal" block>Log out</v-btn>
+      </form>
+    </v-card-actions>
+  </v-card>
 </template>
