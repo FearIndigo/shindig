94368b835e829078f2e0052806874f3c83e4138f
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-18 15:33:16 +0800

    flex column for centered page container

--- frontend/src/components/container/CenteredPage.vue
+++ frontend/src/components/container/CenteredPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="d-flex align-center justify-center h-100">
+  <v-container class="d-flex flex-column align-center justify-center h-100">
     <slot />
   </v-container>
 </template>
