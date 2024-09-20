5b859dbfb99fcfb7e5e20ed7d4ace8564a88b7b7
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-20 10:47:26 +0800

    Add MultiUserList component

---
+++ frontend/src/components/input/MultiUserList.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-autocomplete
+    v-model="users"
+    :items="allUsers"
+    item-title="name"
+    item-value="id"
+    :label="$props.label"
+    prepend-inner-icon="mdi-account-star-outline"
+    multiple
+    chips
+  ></v-autocomplete>
+</template>
+
+<script setup lang="ts">
+import type { UserDocument, UserType } from "~/rxdb/types";
+
+const users = defineModel<string[]>();
+const props = defineProps<{
+  label?: string;
+  excludeUsers?: string[];
+}>();
+
+const allUsers = await useRxQuery<UserType, UserDocument[]>(
+  "users",
+  (collection) =>
+    collection.find({
+      selector: {
+        id: { $nin: props.excludeUsers },
+      },
+    })
+);
+</script>
