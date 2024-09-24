9bedf2380ab7a4a2088f875a817753f583cc42a6
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-25 00:42:30 +0800

    update footer styling

--- frontend/src/components/layout/Footer.vue
+++ frontend/src/components/layout/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer class="d-flex flex-column flex-grow-0">
+  <v-footer class="d-flex flex-column flex-grow-0" elevation="4">
     <p>{{ new Date().getFullYear() }} - <strong>Shindig</strong></p>
   </v-footer>
 </template>
