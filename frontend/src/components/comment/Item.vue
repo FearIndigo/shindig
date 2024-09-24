d99d9c138efac986f4a5c8dcb4706347844adb3a
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-24 10:53:24 +0800

    Add comment list and comment item placeholder

---
+++ frontend/src/components/comment/Item.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-list-item>{{ comment }}</v-list-item>
+</template>
+
+<script setup lang="ts">
+import type { CommentDocument } from "~/rxdb/types";
+defineProps<{ comment: CommentDocument }>();
+</script>
---
+++ frontend/src/components/comment/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <p v-if="comments.length === 0" class="text-disabled">
+    No comments to display...
+  </p>
+  <v-list v-else class="pa-0" lines="three" rounded>
+    <template v-for="(comment, i) in $props.comments" :key="comment.id">
+      <CommentItem :comment="comment" />
+      <v-divider
+        v-if="i < comments!.length - 1"
+        :key="`${i}-divider`"
+      ></v-divider>
+    </template>
+  </v-list>
+</template>
+
+<script setup lang="ts">
+import type { CommentDocument } from "~/rxdb/types";
+defineProps<{ comments: CommentDocument[] }>();
+</script>
